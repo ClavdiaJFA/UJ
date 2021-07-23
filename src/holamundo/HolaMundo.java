45cda3c640e0c04e85fc871f120b27907f166ead
ClavdiaJFA <cjfa08102013@gmail.com>
2021-07-23 08:38:32 -0500

    Primeros pasos curso Universidad Java 2021

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+//Mi clase en Java
+public class HolaMundo {
+ 
+    public static void main(String[] args) {
+        
+        
+       /* Scanner scanner = new Scanner(System.in);
+        System.out.println("Proporciona el título: ");
+        String titulo = scanner.nextLine();
+        System.out.println("Proporciona el autor: ");
+        String autor = scanner.nextLine();
+        System.out.println("fue escrito por: ");
+        System.out.println(titulo + "fue escrito por " + autor); */
+       
+       //2do módulo: Concatenación de cadenas
+       // el "\n" es un salto de línea
+       // el "\t" es un tabulador en la línea
+       // el "\b" es un retroceso en la línea
+      
+       /*
+       String usuario = "Juan";
+       String titulo = "Ingeniero";
+       
+       String union = titulo + " " + usuario;
+       System.out.println("union = " + union);
+       
+       int i = 3;
+       int j = 4;
+       
+        System.out.println(i + j ); //se realiza la suma de números
+        System.out.println(i + j + usuario); //Evaluacion de izq a der realiza suma
+        System.out.println(usuario + i + j); //contexto cadena, todo es una cadena
+        System.out.println(usuario + (i + j)); //la variable Juan se está concatenando con el valor de i y de j
+        //En este último el uso de paréntesis modifica la prioridad en la evaluación
+       */
+        
+        /* Primeros pasos del curso. 1er módulo
+        
+        //Definimos la variable
+        int miVariableEntera = 10; //literal numerico: tipo int
+        System.out.println(miVariableEntera);
+        //Modificando el valor de la variable
+       miVariableEntera = 5;
+       //escribir sout + tabulador: autocompletado
+        System.out.println(miVariableEntera);
+        
+        String miVariableCadena = "Saludos"; //literal dacena: tipo String
+        //mi + ctrl + space: muestra las variables y se selecciona la que se va a utilizar
+        System.out.println(miVariableCadena);
+        
+        miVariableCadena = "Adios";
+        System.out.println(miVariableCadena);
+        
+       
+        //var - Inferencia de tipos en Java (var: ´versiones 10 en adelante)
+        int miVariableEntera2 = 15; //int: entera
+        System.out.println(miVariableEntera2);
+        
+        String miVariableCadena2 = "Nueva Cadena"; //String: cadena
+        System.out.println("miVariableCadena2 - " + miVariableCadena2); 
+        
+        */
+        
+    }
+    
+}
